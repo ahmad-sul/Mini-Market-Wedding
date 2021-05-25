d057e4f1c66df5343d319c9392409d2fea3afbd4
Ahmad <ahhk.48@gmail.com>
2021-05-25 15:40:24 +0200

    add image

--- src/component/Home.js
+++ src/component/Home.js
@@ -23,7 +23,7 @@ render() {
     </div>
     
          <div className="col-md-6 text-center">
-                       <img src={image1}  className="homeImage img img-fluid  " alt="" />
+                       <img src='https://scontent-ber1-1.xx.fbcdn.net/v/t1.6435-1/122940660_3017756205157079_1922317843292772306_n.jpg?_nc_cat=107&ccb=1-3&_nc_sid=dbb9e7&_nc_ohc=MfFUXSItyJ4AX8tKsB3&_nc_oc=AQl9CszpUDFKQk4-eDs7GekIb_o5JHXb5a-WtseZruDSLrznVKS7z0sO1osd24D5Ps6AbTBk4oXqnj2SWuRGuadn&_nc_ht=scontent-ber1-1.xx&oh=a6db854c7e42a278112676daa9e094ed&oe=60D0C338' className="homeImage img img-fluid  " alt="" />
                    </div>
         </div>
         </div>
